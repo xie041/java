f4602dfc163bd0677f8a29f046bc758df9680d13
xie041 <xie041@126.com>
2012-11-16 10:04:35 +0800

    删除rest目录

--- src/rest/resteasy/demo/guice/src/org/jboss/resteasy/examples/hello/GreeterImpl.java
+++
@@ -1,9 +0,0 @@
-package org.jboss.resteasy.examples.hello;
-
-public class GreeterImpl implements IGreeter
-{
-   public String greet(final String name)
-   {
-      return "Hello " + name;
-   }
-}
--- src/rest/resteasy/demo/guice/src/org/jboss/resteasy/examples/hello/IGreeter.java
+++
@@ -1,6 +0,0 @@
-package org.jboss.resteasy.examples.hello;
-
-public interface IGreeter
-{
-   public String greet(final String name);
-}
